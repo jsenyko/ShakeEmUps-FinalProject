79ef4eac13362a0530297ca83bed14be0fd71804
jsenyko <jakobsenyko@gmail.com>
2023-03-31 23:12:23 -0400

    pushIt

---
+++ frontend/src/models/UserDrinkModel.ts
@@ -0,0 +1,9 @@
+export interface UserDrink{
+    newDrinkId: string,
+      strDrink: string,
+      strGlass: string,
+      strIngredient1: string,
+      strMeasure1: string,
+      strInstructions: string,
+      strImageSource: string
+}
